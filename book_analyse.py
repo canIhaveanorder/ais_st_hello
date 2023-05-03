ec8f9c6b5725345c7258507d44778fa5b386cb70
Dokoung <118654142+canIhaveanorder@users.noreply.github.com>
2023-05-03 15:50:41 +0900

    Update book_analyse.py

--- book_analyse.py
+++ book_analyse.py
@@ -20,6 +20,35 @@ df_train = pd.read_csv('data/AGE.csv')
 st.dataframe(df_train)
 
 
-
-
 js = "window.scrollTo(0, document.getElementById('part-1-age-analysis').offsetTop);"
+
+
+st.markdown("<h3 id='part-1-age-analysis'>Part 1. 도서 분석</h3>", unsafe_allow_html=True)
+
+books =  train[["Book-ID","Book-Title", "Book-Author", "Year-Of-Publication"]].drop_duplicates().merge(number_of_book_ratings, on="Book-ID")
+books = books.join(average_book_rating, on="Book-ID")
+books
+
+
+popbooks = books.sort_values(by="N_ratings", ascending=False).nlargest(10, 'N_ratings')
+popbooks
+
+
+from matplotlib import pyplot as plt
+from matplotlib import rcParams
+
+rcParams['figure.figsize'] = 10, 9
+
+# seaborn 패키지로 barplot 그리기
+sns.barplot(x='N_ratings', y='Book-Title', data=popbooks,palette='Set1')
+
+# x축 레이블 90도 회전
+plt.xticks(rotation=50)
+
+# 그래프 제목 추가
+plt.title('Top10 Books most read by users', fontsize=16)
+
+# 그래프 출력
+plt.show()
+
+
