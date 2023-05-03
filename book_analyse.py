54b3e52447c238ca775a319145ccaa2c35c2c319
Dokoung <118654142+canIhaveanorder@users.noreply.github.com>
2023-05-03 15:43:34 +0900

    Update book_analyse.py

--- book_analyse.py
+++ book_analyse.py
@@ -5,9 +5,21 @@ import numpy as np
 st.header('도서분석')
 st.sidebar.title('도서분석')
 
+st.sidebar.markdown("""
+    ## 도서관련 분석
+    - [part 1. 도서 분석](#part-1-book-analysis)
+    - [part 2. 작가 분석](#part-2-author-analysis)
+    - [part 3. 출판년도 분석](#part-3-publisher-analysis)
+""")
+
+
 st.write("""
 ### Dataset
 """)
-df_train = pd.read_csv('data/TRAIN.csv')
+df_train = pd.read_csv('data/AGE.csv')
 st.dataframe(df_train)
 
+
+
+
+js = "window.scrollTo(0, document.getElementById('part-1-age-analysis').offsetTop);"
