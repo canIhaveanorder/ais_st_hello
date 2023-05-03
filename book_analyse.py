7d9c7fc2d3583ca8993d64bebd779a5bdf2be84f
Dokoung <118654142+canIhaveanorder@users.noreply.github.com>
2023-05-03 15:38:22 +0900

    Update book_analyse.py

--- book_analyse.py
+++ book_analyse.py
@@ -5,12 +5,9 @@ import numpy as np
 st.header('고객분석')
 st.sidebar.title('고객분석')
 
+st.write("""
 ### Dataset
 """)
 df_train = pd.read_csv('data/AGE.csv')
 st.dataframe(df_train)
 
-
-
-
-js = "window.scrollTo(0, document.getElementById('part-1-age-analysis').offsetTop);"
