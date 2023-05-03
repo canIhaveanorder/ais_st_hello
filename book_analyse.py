d5a98bf7c783470a6e31fd7c1f0566843d168810
Dokoung <118654142+canIhaveanorder@users.noreply.github.com>
2023-05-03 15:39:48 +0900

    Update book_analyse.py

--- book_analyse.py
+++ book_analyse.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header('고객분석')
-st.sidebar.title('고객분석')
+st.header('도서분석')
+st.sidebar.title('도서분석')
 
 st.write("""
 ### Dataset
 """)
-df_train = pd.read_csv('data/AGE.csv')
+df_train = pd.read_csv('data/TRAIN.csv')
 st.dataframe(df_train)
 
